518d3f607ef836ba351fca64ff5e47439f4062e4
João Monteiro <joaomiguelmonteiro@gmail.com>
2018-05-11 23:00:17 +0100

    Added medhelp

--- src/MedHelp.java
+++ src/MedHelp.java
@@ -11,8 +11,36 @@ public class MedHelp {
     public static void workit() throws IOException {
         PrintWriter writer = new PrintWriter("medhelp.txt", "UTF-8");
 
-        for(int i = 1; i < 137; i++) {
-            crawl("https://forumsaude.com/conversas/pagina-" + i, writer);
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Diabetes---Type-1/show/220?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Pregnancy-Ages-18-24-/show/152?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Exercise--Fitness/show/69?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Anxiety/show/71?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Depression/show/57?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Heart-Disease/show/72?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 42; i++) {
+            crawl("https://www.medhelp.org/forums/High-Blood-Pressure---Hypertension/show/1222?page=" + i, writer);
+        }
+
+        for(int i = 1; i < 150; i++) {
+            crawl("https://www.medhelp.org/forums/Heart-Rhythm/show/92?page=" + i, writer);
         }
 
         writer.close();
@@ -22,11 +50,12 @@ public class MedHelp {
         Document doc = Jsoup.connect(url).get();
         System.out.println(url);
 
-        Elements discussions = doc.select("#Discussions .Discussion");
+        Elements discussions = doc.select(".subj_title");
         System.out.println(discussions.size());
 
         for (Element discussion : discussions) {
-            checkContents(discussion.select(".DiscussionTopic a").attr("href"), writer);
+            Elements elems = discussion.select("a");
+            checkContents(elems.get(0).absUrl("href"), writer);
             //writer.println("%s\n\t%s" + headline.attr("title") + headline.absUrl("href"));
         }
     }
@@ -34,16 +63,10 @@ public class MedHelp {
     private static void checkContents(String url, PrintWriter writer) throws IOException {
         Document thread = Jsoup.connect(url).get();
 
-        writer.println(thread.select(".ContentInfo h1 a").attr("title"));
-
-        Elements comments = thread.select("#Comments  li");
-
-        for (Element comment : comments) {
-            String attempt = comment.select(".CommentBody p").toString();
-            attempt = attempt.replace("<p>", "");
-            attempt = attempt.replace("</p>", "");
-            attempt = attempt.replace("<br>", "");
-            writer.println(attempt);
-        }
+        writer.println(thread.select(".subj_title").text());
+        writer.println(thread.select("#subject_msg").text());
+        String attempt = thread.select(".resp_body").text();
+        //attempt = attempt.replace("<br/>", "");
+        writer.println(attempt);
     }
 }
